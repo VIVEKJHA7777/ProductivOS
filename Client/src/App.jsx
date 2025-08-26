3078ccd40e884301e34c1842d62002f2ffa45bf6
vivekjha07777 <vivekjha07777@gmail.com>
2025-08-26 20:42:43 +0530

    Initial Server Setup

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css";
+import axios from 'axios';
+import { useEffect,useState } from 'react';
+import SidebarLayout from "./layouts/SidebarLayout";
+
+const App = () => {
+  return (
+    <SidebarLayout/>
+  )
+}
+
+export default App
---
+++ Client/src/layouts/SidebarLayout.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import AddTaskRoundedIcon from '@mui/icons-material/AddTaskRounded';
+import OutlinedFlagRoundedIcon from '@mui/icons-material/OutlinedFlagRounded';
+import QueryStatsRoundedIcon from '@mui/icons-material/QueryStatsRounded';
+import AccessAlarmsRoundedIcon from '@mui/icons-material/AccessAlarmsRounded';
+import CalendarMonthRoundedIcon from '@mui/icons-material/CalendarMonthRounded';
+import BarChartRoundedIcon from '@mui/icons-material/BarChartRounded';
+import ClearRoundedIcon from '@mui/icons-material/ClearRounded';
+
+const SidebarLayout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="flex">
+      {/* Sidebar toggle button (mobile only) */}
+      <button
+        onClick={() => setSidebarOpen(!sidebarOpen)}
+        aria-controls="default-sidebar"
+        type="button"
+        className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-400 rounded-lg sm:hidden hover:text-gray-900 dark:hover:cursor-pointer hover:cursor-pointer focus:ring-gray-600"
+      >
+        <span className="sr-only">Open sidebar</span>
+        <svg
+          className="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clipRule="evenodd"
+            fillRule="evenodd"
+            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+          />
+        </svg>
+      </button>
+
+      {/* Sidebar */}
+      <aside
+        id="default-sidebar"
+        className={`fixed flex justify-center items-center top-0 left-0 z-40 w-65 sm-w-60 h-screen transition-transform bg-[#232428] dark:bg-[#232428] ${
+          sidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } sm:translate-x-0`}
+        aria-label="Sidebar"
+      >
+        <div className="h-full w-full sm:w-[90%] px-3 py-4 overflow-y-auto flex flex-col">
+          <div className="flex justify-center items-center gap-3">
+          <h2 className="text-3xl py-2 text-amber-600 font-semibold flex gap-1">ProductivOS <i className="text-blue-500"> <BarChartRoundedIcon/> </i></h2>
+          <i className="self-end inline-block sm:hidden text-gray-100 py-2"><ClearRoundedIcon sx={{fontSize:30}}/></i>
+          </div>
+          <ul className="space-y-2 font-medium">
+            {[
+              {
+                label:'Dashboard', 
+                icon: <QueryStatsRoundedIcon/>
+              },
+              {
+                label:'Tasks', 
+                icon: <AddTaskRoundedIcon/>,
+                badge: 'Pro'
+              },
+              {
+                label:'Goals', 
+                icon: <OutlinedFlagRoundedIcon/>,
+                badge: 3
+              },
+              {
+                label:'Time', 
+                icon: <AccessAlarmsRoundedIcon/>
+              },
+              {
+                label:'Calender', 
+                icon: <CalendarMonthRoundedIcon/>
+              },
+            ].map((item,idx)=>(
+              <li key={idx}>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-100 rounded-lg hover:bg-gray-600 dark:hover:bg-gray-100 hover:text-white group"
+              >
+                {item.icon}
+                <span className="ms-3">{item.label}</span>
+                 {item.badge && (
+                    <span
+                      className={"inline-flex items-center justify-center px-2 ms-3 text-sm font-medium rounded-full text-gray-100 bg-gray-600 dark:bg-gray-600 dark:text-gray-300"}
+                    >
+                      {item.badge}
+                    </span>
+                  )}
+              </a>
+            </li>
+            ))}
+          </ul>
+        </div>
+      </aside>
+    </div>
+  );
+};
+
+export default SidebarLayout;
+
